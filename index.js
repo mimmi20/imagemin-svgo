33b501e74c5364bd92ac19d5d8873c554bfb75be
Thomas Müller <mimmi20@live.de>
2025-06-21 13:02:50 +0200

    fix issues

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-import {Buffer} from 'node:buffer';
+import { Buffer } from 'node:buffer';
 import isSvg from 'is-svg';
-import {optimize} from 'svgo';
+import { optimize } from 'svgo';
 
-const imageminSvgo = options => async buffer => {
-	options = {
-		multipass: true,
-		...options,
-	};
+const imageminSvgo = (options) => async (buffer) => {
+  options = {
+    multipass: true,
+    ...options,
+  };
 
-	const normalizedInput = Buffer.isBuffer(buffer) ? buffer.toString() : buffer;
+  const normalizedInput = Buffer.isBuffer(buffer) ? buffer.toString() : buffer;
 
-	if (!isSvg(normalizedInput)) {
-		return buffer;
-	}
+  if (!isSvg(normalizedInput)) {
+    return buffer;
+  }
 
-	const {data} = optimize(normalizedInput, options);
-	return Buffer.from(data);
+  const { data } = optimize(normalizedInput, options);
+  return Buffer.from(data);
 };
 
 export default imageminSvgo;
--- test.js
+++ test.js
@@ -1,30 +1,33 @@
 import test from 'ava';
 import imageminSvgo from './index.js';
 
-test('optimize a SVG', async t => {
-	const buffer = await imageminSvgo()('<svg><script></script></svg>');
+test('optimize a SVG', async (t) => {
+  const buffer = await imageminSvgo()('<svg><script></script></svg>');
 
-	t.is(buffer.toString(), '<svg><script/></svg>');
+  t.is(buffer.toString(), '<svg><script/></svg>');
 });
 
-test('support SVGO options', async t => {
-	const buffer = await imageminSvgo({
-		plugins: [{
-			name: 'preset-default',
-		}, {
-			name: 'removeScriptElement',
-			active: true,
-		}],
-	})('<svg><script></script></svg>');
+test('support SVGO options', async (t) => {
+  const buffer = await imageminSvgo({
+    plugins: [
+      {
+        name: 'preset-default',
+      },
+      {
+        name: 'removeScriptElement',
+        active: true,
+      },
+    ],
+  })('<svg><script></script></svg>');
 
-	t.is(buffer.toString(), '<svg/>');
+  t.is(buffer.toString(), '<svg/>');
 });
 
 // Failing as SVGO doesn't throw proper errors...
-test.failing('error on corrupt SVG', async t => {
-	await t.throwsAsync(imageminSvgo()('<svg>style><</style></svg>'), {message: /Error in parsing SVG/});
+test.failing('error on corrupt SVG', async (t) => {
+  await t.throwsAsync(imageminSvgo()('<svg>style><</style></svg>'), { message: /Error in parsing SVG/ });
 });
 
-test('ignore non valid SVG', async t => {
-	t.is(await imageminSvgo()('<html></html>'), '<html></html>');
+test('ignore non valid SVG', async (t) => {
+  t.is(await imageminSvgo()('<html></html>'), '<html></html>');
 });
